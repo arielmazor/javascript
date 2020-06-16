6d14f33e836aafe2f3a79e983056ecf7933d2eef
ARIEL MAZOR <ariel.mazor.mail.com>
2020-06-16 22:20:49 +0300

    ...

--- friendsNum/friendsNum.js
+++ friendsNum/friendsNum.js
@@ -1,24 +1,35 @@
 function getDevivde() {
-  var x1 = 0;
-  var y1 = 0;
-  var x = $('.input1').val();
-  var y = $('.input2').val();
-  for (var i = 1; i < x; i++) {
-    if (x % i == 0) {
-      x1 += i;
-    }
-  }
-  for (var i = 1; i < y; i++) {
-    if (y % i == 0) {
-      y1 += i;
-    }
-  }
-
-  if (x1 == y && y1 == x) {
-    alert(`the number: ${x} is friend number to number: ${y}`);
+  var Num1 = parseInt($(".num1 .Num-input").val());
+  var Num2 = parseInt($(".num2 .Num-input").val());
+  var resulte1 = 0;
+  var resulte2 = 0;
+  if(!_.isFinite(Num1)){
+    $(".input-wrapper.num1").addClass("red");
   }else{
-    alert(`the number: ${x} is not friend number to number: ${y}`);
+    $(".input-wrapper.num1").removeClass("red");
   }
+  if(!_.isFinite(Num2)){
+    $(".input-wrapper.num2").addClass("red");
+  }else{
+    $(".input-wrapper.num2").removeClass("red");
+  }
+  for (var i = 1; i < Num1; i++) {
+    if (Num1 % i == 0) {
+      resulte1 += i;
+    }
+  }
+  for (var i = 1; i < Num2; i++) {
+    if (Num2 % i == 0) {
+      resulte2 += i;
+    }
+  }
+  // if (Num1 > 0) {
+  //   if (Num2 > 0) {
+  //     if (Num1 == Num2 && Num1) {
+  //       alert(`the number: ${Num1} is friend number to number: ${Num2}`);
+  //     } else {
+  //       alert(`the number: ${Num1} is not friend number to number: ${Num2}`);
+  //     }
+  //   }
+  // }
 }
-
-
