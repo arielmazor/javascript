b67e4be0afb41130a2271c0efecfdcb6818ab0fe
ARIEL MAZOR <ariel.mazor.mail.com>
2020-06-27 22:52:00 +0300

    ...

--- Id/id.js
+++ Id/id.js
@@ -1,66 +1,50 @@
-var usersDate = [
+var obj = [
   {
-    firstName: "shaul",
-    firstName: "mazor",
-    address: "menachem begin 77 cfar yona",
+    src: "men.png",
+    fullName: "Yafa mazor",
+    adrres: "yocneam elite",
+    gender: "men-gender-icon.png",
   },
   {
-    fullName: "Sm",
-    adrres: "menachem begin 77 cfar yona",
-  },
-  { 
-      fullName: "Hm", adrres: "menachem begin 77 cfar yona"
-  },
-  {
-    fullName: "Am",
-    adrres: "menachem begin 77 cfar yona",
-  },
-  {
-    fullName: "Rm",
-    adrres: "menachem begin 77 cfar yona",
-  },
-  {
-    fullName: "Ym",
-    adrres: "yocneam elite ",
-  },
-  {
+    src:
+      "https://s.yimg.com/ny/api/res/1.2/tKkCUB5xgXhjeL1CzWIfKQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/people_218/fa7c9bfd3d17b7170efd15f840684e0f",
     fullName: "Bh",
     adrres: "cfar saba",
+    gender:
+      "women.png",
   },
   {
+    src:
+      "https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png",
     fullName: "mm",
     adrres: "criat motzkin",
+    gender: "men-gender-icon.png",
   },
   {
+    src:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png",
     fullName: "Rm",
     adrres: "harzelia",
+    gender: "women.png",
   },
 ];
 
-function build(num) {
-
-    if(num == 1){
-        document.querySelector('.name_id1').innerHTML = usersDate[0].fullName;
-        document.querySelector('.adrres_id1').innerHTML = usersDate[0].adrres;
-        document.querySelector('.name_id2').innerHTML = usersDate[1].fullName;
-        document.querySelector('.adrres_id2').innerHTML = usersDate[1].adrres;
-        document.querySelector('.name_id3').innerHTML = usersDate[2].fullName;
-        document.querySelector('.adrres_id3').innerHTML = usersDate[2].adrres;
-    }
-    if(num == 2){
-        document.querySelector('.name_id1').innerHTML = usersDate[3].fullName;
-        document.querySelector('.adrres_id1').innerHTML = usersDate[3].adrres;
-        document.querySelector('.name_id2').innerHTML = usersDate[4].fullName;
-        document.querySelector('.adrres_id2').innerHTML = usersDate[4].adrres;
-        document.querySelector('.name_id3').innerHTML = usersDate[5].fullName;
-        document.querySelector('.adrres_id3').innerHTML = usersDate[5].adrres;
-    }
-    if(num == 3){
-        document.querySelector('.name_id1').innerHTML = usersDate[6].fullName;
-        document.querySelector('.adrres_id1').innerHTML = usersDate[6].adrres;
-        document.querySelector('.name_id2').innerHTML = usersDate[7].fullName;
-        document.querySelector('.adrres_id2').innerHTML = usersDate[7].adrres;
-        document.querySelector('.name_id3').innerHTML = usersDate[8].fullName;
-        document.querySelector('.adrres_id3').innerHTML = usersDate[8].adrres;
-    }
+function build() {
+  let template = _.template($("#script-list").html());
+  $(".id-wrapper").html(template(obj));
 }
+
+function showModal(index) {
+  let template = _.template($("#contant-script").html());
+  $(".modal .content .info-wrapper").html(template(obj[index]));
+
+  $(".modal").addClass("show");
+  $(".contant").addClass("show");
+}
+
+function closeModal() {
+  $(".modal").removeClass("show");
+  $(".contant").removeClass("show");
+}
+
+build();
