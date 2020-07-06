037943631a5283ea4aad41616e2e86e1392e52c2
ARIEL MAZOR <ariel.mazor.mail.com>
2020-07-06 15:45:59 +0300

    ...

--- contacts/JavaScript/contacts.js
+++ contacts/JavaScript/contacts.js
@@ -3,41 +3,41 @@ var obj = [
     src:
       "https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png",
     fullName: "Yafa mazor",
-    adrres: "yocneam elite",
+    address: "yocneam elite",
     gender: "male",
     phoneNumber:"972526543631",
-    delete:"delete.png",
-    edit:"edit.png"
+    delete:"../Images/delete.png",
+    edit:"../Images/edit.png"
   },
   {
     src:
       "https://s.yimg.com/ny/api/res/1.2/tKkCUB5xgXhjeL1CzWIfKQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/people_218/fa7c9bfd3d17b7170efd15f840684e0f",
     fullName: "Bh",
-    adrres: "cfar saba",
+    address: "cfar saba",
     gender: "fimale",
     phoneNumber:"972526543631",
-    delete:"delete.png",
-    edit:"edit.png"
+    delete:"../Images/delete.png",
+    edit:"../Images/edit.png"
   },
   {
     src:
       "https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png",
     fullName: "mm",
-    adrres: "criat motzkin",
+    address: "criat motzkin",
     gender: "male",
     phoneNumber:"972526543631",
-    delete:"delete.png",
-    edit:"edit.png"
+    delete:"../Images/delete.png",
+    edit:"../Images/edit.png"
   },
   {
     src:
       "https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png",
     fullName: "Rm",
-    adrres: "harzelia",
+    address: "harzelia",
     gender: "fimale",
     phoneNumber:"972526543631",
-    delete:"delete.png",
-    edit:"edit.png"
+    delete:"../Images/delete.png",
+    edit:"../Images/edit.png"
   },
 ];
 
@@ -51,13 +51,12 @@ function editRow(){
 function showModal(index) {
 
    let template = _.template($("#contant").html());
-   $(".modal .content .info-wrapper").html(template(obj[index]));
+   $(".modal .content .body").html(template(obj[index]));
 
-  $(".modal").addClass("show");
-  $(".content").addClass("show");
+  $(".modal").addClass("show1");
 }
 
 function closeModal() {
-  $(".modal").removeClass("show");
+  $(".modal").removeClass("show1");
 }
 build();
